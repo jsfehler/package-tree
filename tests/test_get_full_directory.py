6cf5455587c37fe53cff27a1e8db5a10ee2e846b
Joshua Fehler <jsfehler@gmail.com>
2018-09-04 16:38:55 -0400

    Refactoring to support more directory structures

---
+++ tests/test_get_full_directory.py
@@ -0,0 +1,27 @@
+from packagetree import PackageTree
+
+
+def test_get_full_directory():
+    """Ensure _get_full_directory combines the module, root, and directory
+    correctly."""
+    x = PackageTree._get_full_directory(
+        None,
+        module="zab",
+        root=None,
+        directory="foo/bar/biz/bin/bat"
+    )
+
+    assert "foo/bar/biz/bin/bat/zab" == x
+
+
+def test_get_full_directory_nested_module():
+    """Ensure _get_full_directory combines the module, root, and directory
+    correctly."""
+    x = PackageTree._get_full_directory(
+        None,
+        module="zab.zim",
+        root=None,
+        directory="foo/bar/biz/bin/bat"
+    )
+
+    assert "foo/bar/biz/bin/bat/zab/zim" == x
